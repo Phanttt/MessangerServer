593c59cd7e499ee847deec243df383f8baba32c8
Phanttt <117937309+Phanttt@users.noreply.github.com>
2023-02-26 02:48:44 +0100

    search works

--- Controllers/DataHub.cs
+++ Controllers/DataHub.cs
@@ -16,15 +16,18 @@ namespace MessangerServer.Controllers
         }
         public override async Task OnConnectedAsync()
         {
-            var userid = Context.GetHttpContext().Request.Query["userid"];
-            var chats = await context.AttachmentUserChats.Where(x => x.UserId == Convert.ToInt32(userid)).ToListAsync();
+            var userid = Convert.ToInt32(Context.GetHttpContext().Request.Query["userid"]);
+            var chats = await context.AttachmentUserChats.Where(x => x.UserId == userid).ToListAsync();
 
             foreach (var item in chats)
             {
-                Console.WriteLine("User" + userid + ": +" + "Group" + item.ChatId);
                 await Groups.AddToGroupAsync(Context.ConnectionId, "Group"+item.ChatId);
             }
-            
+
+            var user = await context.Users.FirstOrDefaultAsync(e=>e.Id == userid);
+            user.Status = true;
+            await context.SaveChangesAsync();
+
             await base.OnConnectedAsync();
         }
         public async Task SendMessage(ChatMessage message)
@@ -44,8 +47,6 @@ namespace MessangerServer.Controllers
             curchat.Messages.Add(newm);
             await context.SaveChangesAsync();
 
-            newm = await context.Messages.OrderByDescending(e=>e.DispatchTime).FirstOrDefaultAsync(e=>e.ChatId == message.ChatId);
-
             await Clients.Group("Group" + message.ChatId).SendAsync("ReceiveMessage", newm);
         }
         public async Task GetChats(string currUserId)
@@ -60,11 +61,20 @@ namespace MessangerServer.Controllers
         }
         public async Task getChatMessages(int currchatId)
         {
-            //var currchat = await context.Chats.Include(e => e.Users).Include(e => e.Messages).FirstOrDefaultAsync(e => e.Id == currchatId);
             var messages = await context.Messages.Where(e => e.ChatId == currchatId).Include(e=>e.Sender).OrderByDescending(e=>e.DispatchTime).ToListAsync();
             await Clients.Caller.SendAsync("CurrentChatMessages", messages);
         }
 
-
+        public async Task searchResult(string str)
+        {
+            if (!string.IsNullOrEmpty(str)){
+                var foundUsers = await context.Users.Where(e => e.Login.StartsWith(str)).ToListAsync();
+                await Clients.Caller.SendAsync("SearchResult", foundUsers);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("SearchResult", null);
+            }
+        }
     } 
 }
